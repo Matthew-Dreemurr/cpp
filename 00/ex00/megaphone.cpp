f6719863611d8c52307831051d9aba49aeb1f05a
matthew-dreemurr <matth.hadad@gmail.com>
2023-05-01 14:29:17 +0200

    [FIX] CPP00ex00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 
-int main(int argc, const char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
