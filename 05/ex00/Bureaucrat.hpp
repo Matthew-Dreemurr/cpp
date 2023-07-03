3f263225334501dcae68387df314c11a19021ebb
matthew-dreemurr <hadadmat@gmail.com>
2023-07-03 16:37:32 +0200

    [WIP] CPP05ex00

---
+++ 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,46 @@
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <iostream>
+#include <stdexcept>
+
+class Bureaucrat
+{
+	class GradeTooHighException: public std::runtime_error {
+		public:
+			GradeTooHighException(const int grade);
+
+			GradeTooHighException(const std::string & grade);
+
+			GradeTooHighException(const Bureaucrat & data);
+	};
+	class GradeTooLowException: public std::runtime_error {
+		public:
+			GradeTooLowException(const int grade);
+
+			GradeTooLowException(const std::string & grade);
+
+			GradeTooLowException(const Bureaucrat & data);
+	};
+
+private:
+	const std::string	name;
+	int					grade;
+
+public:
+	Bureaucrat(std::string newName, int newGrade);
+	Bureaucrat(const Bureaucrat &newBureaucrat);
+	Bureaucrat*	operator=(Bureaucrat &);
+
+	const std::string& getName() const;
+	const int& getGrade() const;
+
+	void promote();
+	void demote();
+
+	~Bureaucrat();
+};
+
+std::ostream& operator<<(std::ostream &os, const Bureaucrat &data);
+
+#endif /* BUREAUCRAT_H */
