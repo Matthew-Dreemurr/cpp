6d213bf8769f01b2848c6b0b176a9c32fa8a0b62
matthew-dreemurr <hadadmat@gmail.com>
2023-08-21 16:52:37 +0200

    [WIP] CPP08ex00

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -13,4 +13,15 @@ int main () {
 	int_list.push_back(i++);
 
 	std::cout << easyfind<std::list<int> >(int_list, 3) << std::endl;
+
+	char c = 'a';
+
+	std::vector<char>	char_vec;
+	char_vec.push_back(c++);
+	char_vec.push_back(c++);
+	char_vec.push_back(c++);
+	char_vec.push_back(c++);
+	char_vec.push_back(c++);
+
+	std::cout << easyfind<std::vector<char> >(int_list, 99) << std::endl;//TODO FIX
 }
