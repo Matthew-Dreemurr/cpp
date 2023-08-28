5aae2fbdefb8d873d80c593c029c09eee6edfd6b
matthew-dreemurr <hadadmat@gmail.com>
2023-08-28 14:14:34 +0200

    [WIP] CPP09ex01

--- 09/ex01/main.cpp
+++ 09/ex01/main.cpp
@@ -1,15 +1,70 @@
 #include <stack>
 #include <cctype>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <iostream>
 
 int main(int ac, char **av)
 {
-    std::stack<int> s;
+    if (ac < 2) {
+        printf("Error\n");
+        return (1);
+    }
 
-    for (int i = 0; i < ac; i++) {
-        if (!std::isdigit(av[i][0]) && av[i][1] == '\0') {
-            std::cout << "Error: " << av[i] << " is not a number" << std::endl;
+    std::stack<long> s;
+    std::string args(av[1]);
+    std::stringstream ss(args);
+
+    std::string arg;
+    while (std::getline(ss, arg, ' ')) {
+        // Check if the string as only one char
+        if (arg.size() != 1) {
+            std::cout << "Error" << std::endl; 
             return (1);
         }
+
+        const char c = arg[0];
+
+        // Check if the char is a digit
+        if (std::isdigit(c)) {
+            s.push(c - '0');
+            continue;
+        }
+
+        const char op[] = "+-*/";
+        const char *o = std::strchr(op, c);
+
+        // Check if the char is an operator
+        if (!o) {
+            printf("Error\n");
+            return (1);
+        }
+
+        if (s.size() < 2) {
+            std::cout << "Not enough values in the stack" << std::endl;
+            return (1);
+        }
+
+        const long b = s.top();
+        s.pop();
+        const long a = s.top();
+        s.pop();
+
+        if (*o == '+') {
+            s.push(a + b);
+        } else if (*o == '-') {
+            s.push(a - b);
+        } else if (*o == '*') {
+            s.push(a * b);
+        } else if (*o == '/') {
+            if (b == 0) {
+                std::cout << "Error: Division by zero" << std::endl;
+                return (1);
+            }
+            s.push(a / b);
+        }
     }
+    std::cout << s.top() << std::endl;
     return (0);
 }
