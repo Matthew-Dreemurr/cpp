118383edf9a23864d3e84c8adfdb959d45cbd8ad
matthew-dreemurr <hadadmat@gmail.com>
2023-08-29 16:25:21 +0200

    [WIP] CPP09ex02 insert

--- 09/ex02/PmergeMe.hpp
+++ 09/ex02/PmergeMe.hpp
@@ -2,62 +2,39 @@
 #define PMERGEME_HPP
 
 #include <cstddef>
+#include <iostream>
+
+#define K_DIV 3
 
 template <typename T>
-void    merge(T* array, size_t start, size_t mid, size_t end) {
-    T* tmp = new T[end - start];
-    size_t start_ = start;
-    size_t mid_ = mid;
-    size_t index = 0;
+void    insert(T & container, const typename T::iterator & begin, const size_t len_div) {
+    typename T::iterator ptr = begin;
 
-    while (start_ < mid && mid_ < end) {
-        if (array[start_] < array[mid_]) {
-            tmp[index] = array[start_];
-            start_++;
+    for (size_t i = 0; i < len_div - 1; i++, ptr++) {
+
+        if (*(ptr + 1) < *ptr) {
+            
+            for (typename T::iterator iptr = begin; iptr != (ptr + 1); iptr++) {
+
+                if (*(ptr + 1) < *iptr) {
+                    size_t tmp = *(ptr+1);
+                    container.erase(ptr + 1);
+                    container.insert(iptr, tmp);
+                }
+            }
+        }
+    }
+}
+
+template <typename T>
+void    PmergeMe(T & container) {
+    for (size_t i = 0; i < container.size(); i += K_DIV) {
+        typename T::iterator a = container.begin() + i;
+        if (container.size() - i >= K_DIV) {
+            insert(container, a, K_DIV);
         } else {
-            tmp[index] = array[mid_];
-            mid_++;
+            insert(container, a, container.size() - i);
         }
-        index++;
-    }
-    while (start_ < mid) {
-        tmp[index] = array[start_];
-        start_++;
-        index++;
-    }
-    while (mid_ < end) {
-        tmp[index] = array[mid_];
-        mid_++;
-        index++;
-    }
-    for (size_t i = start; i < end; i++) {
-        array[i] = tmp[i - start];
-    }
-    delete[] tmp;
-}
-
-template <typename T>
-void    insertionSort(T* array, size_t start, size_t end) {
-    for (size_t i = start + 1; i < end; i++) {
-        T tmp = array[i];
-        size_t j = i;
-        while (j > start && array[j - 1] > tmp) {
-            array[j] = array[j - 1];
-            j--;
-        }
-        array[j] = tmp;
-    }
-}
-
-template <typename T>
-void    PmergeMe(T* array, size_t start, size_t end) {
-    if (end - start > 11) {
-        int mid = (start + end) / 2;
-        PmergeMe(array, start, mid);
-        PmergeMe(array, mid, end);
-        merge(array, start, mid, end);
-    } else {
-        insertionSort(array, start, end);
     }
 }
 
--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -5,10 +5,20 @@
 #include <vector>
 
 int main () {
-    int a[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
-    PmergeMe(a, 0, 10);
-    for (int i = 0; i < 10; i++) {
-        std::cout << a[i] << std::endl;
+    int a[] = { 1, 3, 10, 7, 9, 2, 4, 6, 8, 11, 5 };
+    std::vector<size_t> vec;
+
+    for (int i = 0; i < 11; i++) {
+        vec.push_back(a[i]);
+    }
+
+    PmergeMe(vec);
+    std::vector<size_t>::iterator it = vec.begin();
+    std::vector<size_t>::iterator end = vec.end();
+    for (int i = 0; it != end; it++, i++) {
+        if ((i % K_DIV) == 0)
+            std::cout << "---" << std::endl;
+        std::cout << a[i] << " | " << *it << std::endl;
     }
     return 0;
 }
