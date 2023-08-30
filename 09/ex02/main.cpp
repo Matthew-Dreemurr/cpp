459342561152100a13a89fa195a5b9e0c75cc1eb
matthew-dreemurr <hadadmat@gmail.com>
2023-08-30 14:45:25 +0200

    [WIP] CPP09ex02 rework main

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -3,31 +3,44 @@
 
 #include <list>
 #include <vector>
-
+#include <typeinfo>
 
 template <typename T>
-void test(T & container) {
-    container.push_back(2);
-    container.push_back(6);
-    container.push_back(1);
-    container.push_back(7);
-    container.push_back(4);
-    container.push_back(10);
-    container.push_back(9);
-    container.push_back(5);
-    container.push_back(3);
-    container.push_back(8);
+void test(T &container, std::string type, char debug) {
+    std::clock_t    start;
 
-    print_(container);
+    if (debug == 'y') {
+        std::cout << "Before: ";
+        print_(container);
+        std::cout << std::endl;
+    }
+
+
+    start = std::clock();
+
     PmergeMe(container);
-    print_(container);
+    std::cout << "[" << type << "]" << " Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms for " << container.size() << " elements." << std::endl;
 
-    std::cout << "===============" << std::endl;
+    if (debug == 'y') {
+        std::cout << "After: " << std::endl;
+        print_(container);
+        std::cout << std::endl;
+    }
+
 }
 
-int main () {
+int main (int ac, char **av) {
+    if (ac < 3 || (av[1][0] != 'y' && av[1][0] != 'n')) {
+        std::cout << "Usage: ./a.out debug: [n|y] int array: [int, ...]" << std::endl;
+        return (1);
+    }
     std::vector<size_t> vec;
-    test(vec);
     std::vector<size_t> list;
-    test(list);
+    // Convert and store all av in vec
+    for (int i = 2; i < ac; i++) {
+        vec.push_back(std::atoi(av[i]));
+        list.push_back(std::atoi(av[i]));
+    }
+    test(vec, "Vector", av[1][0]);
+    test(list, "List", av[1][0]);
 }
